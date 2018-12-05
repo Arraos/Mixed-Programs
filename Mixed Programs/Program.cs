ddb11144f43c3b51b8d34bcd353c758d6e639571
Wdrożenia <m33how.cs@gmail.com>
2018-12-06 00:27:31 +0100

    Dodaj pliki projektów.

---
+++ Mixed Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mixed_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
