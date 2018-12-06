df93f651ee7b0b2234f1964599e3f649db793a93
Wdrożenia <m33how.cs@gmail.com>
2018-12-06 09:39:36 +0100

    Added Average

--- Mixed Programs/Program.cs
+++ Mixed Programs/Program.cs
@@ -32,19 +32,35 @@ namespace Mixed_Programs
             return c;
         }
     }
-  /*  class Swap
+   class Swap
     {
         public int Podmiana(int a, int b)
         {
-            int c = a + b;
-            Console.WriteLine("Divide by two");
-            Console.WriteLine(c);
-            System.Threading.Thread.Sleep(2000);
-            Console.WriteLine(c);
-            System.Threading.Thread.Sleep(2000);
-            return c;
+            Console.WriteLine("This is your first number: " + a);
+            Console.WriteLine("This is your second number: " + b);
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("This is your first number: " + a);
+            Console.WriteLine("This is your second number: " + b);
+            System.Threading.Thread.Sleep(6000);
+            return 0;
         }
-    } */
+    }
+    class Average
+    {
+        public int Srednia(int a, int b, int c)
+        {
+            int d, e;
+            d = a + b + c;
+            e = d / 3;
+            Console.WriteLine("Suma = " + d);
+            Console.WriteLine("Srednia = " + e);
+            System.Threading.Thread.Sleep(6000);
+            return 0;
+        }
+    }
     class Test
     {
         static void Main(string[] args)
@@ -52,12 +68,21 @@ namespace Mixed_Programs
             Console.WriteLine("Hello World!");
             Addition A = new Addition();
             Divide D = new Divide();
+            Swap S = new Swap();
+            Average Av = new Average();
             int X;
-            int Y;
             Console.WriteLine("GIB ME NUMBER");
             int num1 = int.Parse(Console.ReadLine());
             X = A.Dodawanie(num1);
             X = D.Dzielenie(num1);
+            Console.WriteLine("GIB ME NUMBER TWO");
+            int num2 = int.Parse(Console.ReadLine());
+            X = S.Podmiana(num1,num2);
+            Console.WriteLine("GIB ME NUMBER THREE");
+            int num3 = int.Parse(Console.ReadLine());
+            X = Av.Srednia(num1,num2,num3);
+
+
         }
     }
 }
